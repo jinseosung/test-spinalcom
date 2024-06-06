1cca32493009325b4f206a6df0c36d9aa648469d
Jinseo Sung <jinseosung15@gmail.com>
2024-06-06 11:29:21 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app__container"></div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+.app__container {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  padding: 1.5rem 1rem;
+  background-color: var(--color-grey);
+  color: var(--color-font);
+}
+</style>
