1a93ef75b99b21bdb154baa60be467cdaeca0864
Jinseo Sung <jinseosung15@gmail.com>
2024-06-06 23:04:29 +0200

    add fetchData function

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app__container">
-    <AppHeader />
-    <OccupancyPage />
+    <AppHeader  />
+    <OccupancyPage :buildings="buildings" :floors="floors" />
   </div>
 </template>
 
@@ -12,6 +12,30 @@ import OccupancyPage from "./views/OccupancyPage.vue";
 export default {
   name: "App",
   components: { AppHeader, OccupancyPage },
+  data() {
+    return {
+      buildings: [],
+      floors: [],
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      const API_URL =
+        "https://api-developers.spinalcom.com/api/v1/geographicContext/space";
+      try {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+
+        this.buildings = data.children;
+        this.floors = data.children.map((building) => building.children).flat();
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+  },
 };
 </script>
 
